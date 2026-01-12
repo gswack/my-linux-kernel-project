e6e24724d6ca84b02c19ff68f69a5de1271e7792
swack.gabe@gmail.com <swack.gabe@gmail.com>
2026-01-12 16:37:01 +0200

    Initial commit

--- run_git_commands.bat
+++ run_git_commands.bat
@@ -3,7 +3,7 @@ git add .
 git commit -m "Initial commit"
 @REM gh auth login
 @REM gh repo create my-linux-kernel --public --source=. --remote=origin --push
-git remote add origin https://github.com/gswack/my-linux-kernel-project
+@REM git remote add origin https://github.com/gswack/my-linux-kernel-project
 git remote -v
 git branch -M main
 git push -u origin main
